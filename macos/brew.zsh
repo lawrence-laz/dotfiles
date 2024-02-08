d7e3d9c783bbff66b1f43b41bb851230750dc072
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-02-08 20:24:13 +0200

    chore(brew): Update used packages

--- macos/brew.zsh
+++ macos/brew.zsh
@@ -4,7 +4,65 @@ brew install --cask whichspace
 # For remapping keys
 brew install --cask karabiner-elements
 
+# Triple-tap to invoke mouse middle click
+brew install --cask --no-quarantine middleclick
+
 # Clipboard versioning
 brew install --cask copyq
 
+# Terminal multiplexer
 brew install tmux
+
+# Best editor ever
+brew install nvim
+
+# Process monitor
+brew install htop
+
+# PowerShell
+brew install --cask powershell
+
+# Alternative to `grep`
+brew install ripgrep
+
+# Alternative to `find`
+brew install fd
+
+# Display a list of paths as a tree
+brew install as-tree
+
+# Benchmarking for the command line
+brew install hyperfine
+
+# Utility to count lines of code in a directory
+brew install cloc
+
+# Python
+brew install --caskl anaconda
+
+# Watch
+brew install watch
+
+# File system navigator
+brew install broot
+
+# Cmake build system
+brew install cmake
+
+# Git repo information
+brew install onefetch
+
+# Get process id Utility
+brew install pidof
+
+# Tree viewer
+brew install tree
+
+# Media (video) meta data
+brew install mediainfo
+
+# Core unix tools
+brew install coreutils
+
+# Google Drive
+brew install gdrive
