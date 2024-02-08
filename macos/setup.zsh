5f9aab4df679bf6fbfa70b732a8952744ee9c87b
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-02-08 20:24:01 +0200

    chore(zsh): Add/update mac stuff

--- macos/setup.zsh
+++ macos/setup.zsh
@@ -1,6 +1,8 @@
 source ./defaults.zsh
 
-ln -s ~/git/dotfiles/macos/.zhrc ~/.zhrc
+ln -s ~/git/dotfiles/macos/.zshrc ~/.zshrc
+ln -s ~/git/dotfiles/macos/.zshenv ~/.zshenv
+ln -s ~/git/dotfiles/macos/.zprofile ~/.zprofile
 ln -s ~/git/dotfiles/macos/yabairc ~/.config/yabai/yabairc
 ln -s ~/git/dotfiles/macos/skhdrc ~/.config/skhd/skhdrc
 
@@ -13,4 +15,5 @@ ln -s ~/git/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code
 rm -r ~/.config/kitty
 ln -s ~/git/dotfiles/kitty/ ~/.config/kitty
 
+# Currently mapped all karabiner/ dir
 ln -s ~/git/dotfiles/macos/karabiner.json ~/.config/karabiner/karabiner.json
