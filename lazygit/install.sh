d8d04b5dad7ae89b3ce35e5002fc0ec2d847d3ef
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-02-08 19:57:39 +0200

    chore(lazygit): Get lazygit behave on mac

--- lazygit/install.sh
+++ lazygit/install.sh
@@ -1,1 +1,12 @@
-ln -s ~/git/dotfiles/lazygit/config.yml ~/.config/jesseduffield/lazygit/config.yml
+if [[ $(uname -s) =~ "Darwin" ]]; then
+	# MacOS
+	echo "macos"
+	mkdir -p ~/Library/Application\ Support/lazygit/
+	ln -s ~/git/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+else
+	# Linux
+	echo "linux"
+	mkdir -p ~/.config/jesseduffield/lazygit/
+	ln -s ~/git/dotfiles/lazygit/config.yml ~/.config/jesseduffield/lazygit/config.yml
+fi
+
