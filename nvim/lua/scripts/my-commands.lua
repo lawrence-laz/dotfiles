e2aee4c21e59885c32a926ac969e64f04ad8ad8c
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-07-31 23:16:06 +0300

    chore(nvim): Update my commands

--- nvim/lua/scripts/my-commands.lua
+++ nvim/lua/scripts/my-commands.lua
@@ -1,4 +1,4 @@
-local pickers = require "telescope.pickers"            -- Main module which is used to create a new picker
+local pickers = require "telescope.pickers"            -- Main module which is used to create a new pickermycomm
 local finders = require "telescope.finders"            -- Provides interfaces to fill the picker with items
 local conf = require("telescope.config").values        -- User configuration
 local actions = require "telescope.actions"            -- Holds all actions that can be mapped by a user
@@ -8,6 +8,104 @@ package.loaded['scripts.my-commands'] = nil
 
 local dap_scopes = function()
     local widgets = require 'dap.ui.widgets'; widgets.centered_float(widgets.scopes)
+end
+
+--[[
+
+You could also customize the buffer and window creation using a low-level builder:
+
+>lua
+  local widgets = require('dap.ui.widgets')
+  widgets.builder(widgets.scopes)
+    .new_buf(function_that_creates_and_returns_a_buffer)
+    .new_win(function_that_creates_and_returns_a_window)
+    .build()
+
+
+  vim.wo[winnr].cursorline = true
+
+
+  local bufnr, winnr = lsp_util.open_floating_preview(
+    markdown_lines,
+    'markdown',
+    vim.tbl_extend('keep', win_opt, {
+      focusable = true,
+      focus_id = 'rust-analyzer-hover-actions',
+      close_events = { 'CursorMoved', 'BufHidden', 'InsertCharPre' },
+    })
+  )
+
+
+--]]
+
+local dap_hover_better = function(expr, winopts)
+    local widgets = require 'dap.ui.widgets'
+    local lsp_util = require 'vim.lsp.util'
+
+
+
+    local new_hover_win = function()
+        local bufnr, winnr = lsp_util.open_floating_preview(
+            { 'start' },
+            'markdown',
+            vim.tbl_extend('keep', {}, {
+                focusable = true,
+                focus_id = 'dap-hover',
+                close_events = { 'CursorMoved', 'BufHidden', 'InsertCharPre' },
+            })
+        )
+        return winnr
+    end
+
+    -- new_hover_win();
+
+    -- local value = eval_expression(expr)
+    --
+    local view = widgets.builder(widgets.expression)
+        .new_win(widgets.with_resize(new_hover_win))
+        .build()
+    -- local buf = view.open(value)
+    local buf = view.open()
+    -- api.nvim_buf_set_name(buf, 'dap-hover-' .. tostring(buf) .. ': ' .. value)
+    -- api.nvim_win_set_cursor(view.win, { 1, 0 })
+    return view
+end
+
+local dap_hover = function()
+    -- function M.new_cursor_anchored_float_win(buf)
+    --     vim.bo[buf].bufhidden = "wipe"
+    --     vim.bo[buf].filetype = "dap-float"
+    --     local opts = vim.lsp.util.make_floating_popup_options(50, 30, { border = 'single' })
+    --     local win = api.nvim_open_win(buf, false, opts)
+    --     vim.wo[win].scrolloff = 0
+    --     return win
+    -- end
+    -- vim.lsp.util.open_floating_preview({ "hello" }, "dap-float", {
+    --     focusable = true,
+    --     close_events = { 'CursorMoved', 'BufHidden', 'InsertCharPre' },
+    --     focus_id = 'dappp',
+    --     width = 50,
+    --     height = 5,
+    -- })
+
+    -- if (baar == nil) then
+    baar = require 'dap.ui.widgets'.hover()
+    -- else
+    --     -- vim.api.nvim_set_current_win(baar.win)
+    --     local bufnr, win = vim.lsp.util.open_floating_preview({ "kitas" }, "dap-float", {
+    --         focusable = true,
+    --         -- close_events = { 'CursorMoved', 'BufHidden', 'InsertCharPre' },
+    --         close_events = {},
+    --         focus_id = 'dappp',
+    --         focus = true,
+    --         width = 50,
+    --         height = 5,
+    --     })
+    -- end
+    -- require 'dap.ui.widgets'.scopes
+    -- vim.lsp.buf.hover()
+    -- vim.lsp.util.open_floating_preview()
+    P(vim.api.nvim_list_wins())
 end
 
 local dap_goto = function()
@@ -36,6 +134,20 @@ end
 
 local commands = {
     {
+        name = "Buffer: Close current",
+        exec = "Bdelete",
+        keymap = { "n", "<leader>q" },
+    },
+    {
+        name = "Buffer: Close current FORCE",
+        exec = "Bdelete!",
+        keymap = { "n", "<leader>Q" },
+    },
+    {
+        name = "Buffer: Close all",
+        exec = "bufdo :Bdelete",
+    },
+    {
         name = "Code: Format",
         exec = vim.lsp.buf.format,
         keymap = { "n", "<leader>kd" }
@@ -56,7 +168,8 @@ local commands = {
     },
     {
         name = "Code: Go to definition",
-        exec = "Lspsaga goto_definition",
+        -- exec = "Lspsaga goto_definition", -- Lspsaga keeps saying "request was already sent, please wait".
+        exec = vim.lsp.buf.definition,
         keymap = { "n", "gd" }
     },
     {
@@ -64,20 +177,25 @@ local commands = {
         exec = ':execute "normal \\<Plug>(doge-generate)"',
     },
     -- run to cursor
-    { name = "Debug: Run",               exec = require 'dap'.continue,          keymap = { "n", "drr" } },
-    { name = "Debug: Continue",          exec = require 'dap'.continue,          keymap = { "n", "dc" } },
-    { name = "Debug: Run last",          exec = require 'dap'.run_last,          keymap = { "n", "drl" } },
-    { name = "Debug: Quit",              exec = require 'dap'.close,             keymap = { "n", "dq" } },
-    { name = "Debug: Variables",         exec = dap_scopes,                      keymap = { "n", "dv" } },
-    { name = "Debug: Toggle breakpoint", exec = require 'dap'.toggle_breakpoint, keymap = { "n", "dh" } },
-    { name = "Debug: Step over",         exec = require 'dap'.step_over,         keymap = { "n", "dj" } },
-    { name = "Debug: Run to cursor",     exec = require 'dap'.run_to_cursor,     keymap = { "n", "dJ" } },
-    { name = "Debug: Step out",          exec = require 'dap'.step_over,         keymap = { "n", "dk" } },
-    { name = "Debug: Hover",             exec = require 'dap.ui.widgets'.hover,  keymap = { "n", "dK" } },
-    { name = "Debug: Step into",         exec = require 'dap'.step_into,         keymap = { "n", "dl" } },
-    { name = "Debug: Go to",             exec = dap_goto,                        keymap = { "n", "dL" } },
-    { name = "Debug: Stop",              exec = require 'dap'.stop,              keymap = { "n", "ds" } },
-    { name = "Debug: List breakpoints",  exec = dap_list_breapoints },
+    { name = "Debug: Run",                  exec = require 'dap'.continue,          keymap = { "n", "drr" } },
+    { name = "Debug: Continue",             exec = require 'dap'.continue,          keymap = { "n", "dc" } },
+    { name = "Debug: Run last",             exec = require 'dap'.run_last,          keymap = { "n", "drl" } },
+    { name = "Debug: Quit",                 exec = require 'dap'.terminate,         keymap = { "n", "dq" } },
+    { name = "Debug: Go to current line",   exec = require 'dap'.focus_frame, },
+    { name = "Debug: Open REPL",            exec = require 'dap'.repl.open, },
+    { name = "Debug: Variables (scopes)",   exec = dap_scopes,                      keymap = { "n", "dv" } },
+    { name = "Debug: Toggle breakpoint",    exec = require 'dap'.toggle_breakpoint, keymap = { "n", "dh" } },
+    { name = "Debug: Step over",            exec = require 'dap'.step_over,         keymap = { "n", "dj" } },
+    { name = "Debug: Run to cursor",        exec = require 'dap'.run_to_cursor,     keymap = { "n", "dJ" } },
+    { name = "Debug: Step out",             exec = require 'dap'.step_over,         keymap = { "n", "dk" } },
+    { name = "Debug: Hover",                exec = Dap_better_hover,                keymap = { "n", "dK" } },
+    { name = "Debug: Step into",            exec = require 'dap'.step_into,         keymap = { "n", "dl" } },
+    { name = "Debug: Go to",                exec = dap_goto,                        keymap = { "n", "dL" } },
+    { name = "Debug: Go up in callstack",   exec = require 'dap'.up,                keymap = { "n", "[s" } },
+    { name = "Debug: Go down in callstack", exec = require 'dap'.down,              keymap = { "n", "]s" } },
+    { name = "Debug: Show callstack",       exec = "Telescope dap frames",          keymap = { "n", "dS" } }, -- ds is "delete surrounding"
+    -- { name = "Debug: Stop",              exec = require 'dap'.stop,              keymap = { "n", "ds" } },
+    { name = "Debug: List breakpoints",     exec = dap_list_breapoints },
     {
         name = "Diagnostics: Show for current file",
         exec = "Lspsaga show_buf_diagnostics",
@@ -145,6 +263,42 @@ local commands = {
         exec = "Gitsigns setloclist",
     },
     {
+        -- TODO: Make this a regular gx with universal handlers
+        name = "GitHub: Open link under cursor",
+        exec = function()
+            local current_word = vim.fn.expand('<cWORD>'):gsub('"', ''):gsub(',', '')
+            local github_path = "https://github.com/" .. current_word
+            vim.api.nvim_call_function("netrw#BrowseX", { github_path, 0 })
+        end,
+    },
+    {
+        name = "GitHub: Open link current repo",
+        exec = function()
+            local result = vim.system({ 'git', 'config', '--get', 'remote.origin.url' }, { text = true }):wait()
+            if (result.code ~= 0) then
+                print("Git exited with non-zero result")
+            end
+            local path = result.stdout:gsub("\n", "")
+            vim.api.nvim_call_function("netrw#BrowseX", { path, 0 })
+        end,
+    },
+    {
+        name = "Zig: Build and run",
+        exec = "wall | !zig build run",
+        keymap = { "n", "<leader>zbr" },
+        keymaps = {
+            { "v", "<leader>zbr" }
+        },
+    },
+    {
+        name = "Zig: Build",
+        exec = "!zig build",
+        keymap = { "n", "<leader>zbb" },
+        keymaps = {
+            { "v", "<leader>zbb" }
+        },
+    },
+    {
         name = "Quickfix: Show (toggle)",
         exec = toggle_quickfix,
         keymap = { "n", "gqq" },
@@ -152,12 +306,12 @@ local commands = {
     {
         name = "Quickfix: Older",
         exec = "colder",
-        keymap = { "n", "Q]" },
+        keymap = { "n", "]Q" },
     },
     {
         name = "Quickfix: Newer",
         exec = "cnewer",
-        keymap = { "n", "Q[" },
+        keymap = { "n", "[Q" },
     },
     {
         name = "Quickfix: Clear",
@@ -168,12 +322,12 @@ local commands = {
     {
         name = "Quickfix: Next",
         exec = "cnext",
-        keymap = { "n", "q]" },
+        keymap = { "n", "]q" },
     },
     {
         name = "Quickfix: Previous",
         exec = "cprev",
-        keymap = { "n", "q[" },
+        keymap = { "n", "[q" },
     },
     {
         name = "Quickfix: First",
@@ -224,7 +378,7 @@ local commands = {
         exec = function()
             require("telescope.builtin").diagnostics({ severity_limit = "warn" })
         end,
-        keymap = { "n", "gfe" },
+        -- keymap = { "n", "gfe" }, -- Makes go to fie (gf) slow
     },
     {
         name = "Test: Run nearest",
@@ -232,12 +386,33 @@ local commands = {
             vim.cmd [[w]]
             require("neotest").run.run()
         end,
-        keymap = { "n", "<leader>tr" }
+        keymap = { "n", "<leader>trr" }
     },
     {
-        name = "Test: Show output",
+        name = "Test: Debug",
+        exec = function()
+            vim.cmd [[w]]
+            require("neotest").run.run({ strategy = "dap" })
+        end,
+        keymap = { "n", "<leader>td" }
+    },
+    {
+        name = "Test: Run file",
+        exec = function()
+            vim.cmd [[w]]
+            require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        keymap = { "n", "<leader>trf" }
+    },
+    {
+        name = "Test: Show full output",
         exec = require 'neotest'.output.open,
         keymap = { "n", "<leader>to" }
+    },
+    {
+        name = "Test: Show short output",
+        exec = function() require 'neotest'.output.open({ short = true }) end,
+        keymap = { "n", "<leader>tO" }
     },
     {
         name = "Increment selection",
@@ -253,6 +428,10 @@ local commands = {
     {
         name = "Search help",
         exec = "Telescope help_tags"
+    },
+    {
+        name = "Telescope: Open last window",
+        exec = "Telescope resume",
     },
     {
         name = "Casing: To 'UPPER CASE'",
@@ -331,6 +510,10 @@ local commands = {
         keymap = { "n", "<C-S-Tab>" },
     },
     {
+        name = "Tab: Close",
+        exec = ":tabclose",
+    },
+    {
         name = "Bookmarks: Explore bookmarks",
         exec = require 'bookmarks'.toggle_bookmarks,
         keymap = { "n", "<leader>ke" }
@@ -373,6 +556,14 @@ local commands = {
         name = "View: Current LSP settings",
         exec = "Redir lua P(vim.lsp.get_active_clients())",
     },
+    {
+        name = "SDL3: Open docs",
+        exec = "!open https://wiki.libsdl.org/SDL3/CategoryAPI",
+    },
+    {
+        name = "Zig: Open docs",
+        exec = "!open https://ziglang.org/documentation/master/",
+    },
 
 
     -- TODO: jump to next bookmark
@@ -382,16 +573,45 @@ local commands = {
     --require'bookmarks'.toggle_bookmarks()
 }
 
+local function set_keymap_for_string_command(keymap, string_command)
+    vim.keymap.set(keymap[1], keymap[2], "<cmd>" .. string_command .. "<cr>")
+end
+
+local function set_keymap_for_function_command(keymap, function_command)
+    vim.keymap.set(keymap[1], keymap[2], function_command)
+end
+
 local function set_keymaps()
     for _, command in ipairs(commands) do
-        if command.keymap ~= nil and type(command.exec) == "string" then
-            vim.keymap.set(command.keymap[1], command.keymap[2], "<cmd>" .. command.exec .. "<cr>")
-        elseif command.keymap ~= nil and type(command.exec) == "function" then
-            vim.keymap.set(command.keymap[1], command.keymap[2], command.exec)
+        if type(command.exec) == "string" then
+            if command.keymap ~= nil then
+                set_keymap_for_string_command(command.keymap, command.exec)
+            end
+            if command.keymaps ~= nil then
+                for index, keymap in ipairs(command.keymaps) do
+                    set_keymap_for_string_command(keymap, command.exec)
+                end
+            end
+        elseif type(command.exec) == "function" then
+            if command.keymap ~= nil then
+                set_keymap_for_function_command(command.keymap, command.exec)
+            end
+            if command.keymaps ~= nil then
+                for index, keymap in ipairs(command.keymaps) do
+                    set_keymap_for_function_command(keymap, command.exec)
+                end
+            end
         end
     end
 end
+
+
 set_keymaps()
+
+local rpad = function(s, l, c)
+    local res = s .. string.rep(c or ' ', l - #s)
+    return res, res ~= s
+end
 
 local my_commands = function(opts)
     opts = opts or {}
@@ -400,9 +620,13 @@ local my_commands = function(opts)
         finder = finders.new_table {
             results = commands,
             entry_maker = function(entry)
+                local name = entry.name
+                if name and entry.keymap then
+                    name = rpad(name, 50, ' ') .. "(" .. entry.keymap[2] .. ")"
+                end
                 return {
                     value = entry,
-                    display = entry.name,
+                    display = name,
                     ordinal = entry.name,
                 }
             end
